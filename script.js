e3ff3d886e081df64c8babc2fe982c7caa1cdf12
Adewale Alimi <alimiadewale68@gmail.com>
2022-12-07 10:30:50 -0800

    cleaning up the code

--- script.js
+++ script.js
@@ -7,16 +7,9 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-let initialCount = 5;
-const apiKey = 'jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek';
-let apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${initialCount}`;
-
-// NEW Block****
-function updateAPIURLWithNewCount (picCount) {
-  apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${picCount}`;
-}
-// NEW Block*****
-
+const count = 30;
+const apiKey = 'Nahg-A0ufXY5pGYABXCPZ8eZIdvkMSBqCSor0VYoNuA';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
 // Check if all images were loaded
 function imageLoaded() {
@@ -61,17 +54,12 @@ function displayPhotos() {
   });
 }
 
-ini
 // Get photos from Unsplash API
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
     displayPhotos();
-    if (isInitialLoad) { // NEW LINE ****
-      updateAPIURLWithNewCount(30) // NEW LINE ****
-      isInitialLoad = false // NEW LINE ****
-    } // NEW LINE ****
   } catch (error) {
     // Catch Error Here
   }
